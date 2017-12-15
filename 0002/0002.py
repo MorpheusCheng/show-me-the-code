4a62f96fad5c2cbbc2d980f4ba9db0134bc794be
MorpheusCheng <1203969904cheng@gmail.com>
2017-12-15 22:34:26 +0800

    改了一下，原来keys和values都是关键词

--- 0002/0002.py
+++ 0002/0002.py
@@ -2,10 +2,10 @@
 import  pymysql,random,string
 
 create_table_sql=""" create table secret_keys(
-                        keyss int,valuess char(30))"""
+                       my_keys int,my_values char(30))"""
 
-insert_table_sql="""insert into secret_keys(keyss,valuess)
-                    values('{keyss}','{valuess}')"""
+insert_table_sql="""insert into secret_keys(my_keys,my_values)
+                    values('{keys}','{values}')"""
 
 def activation_code(count,length):
     base=string.ascii_letters+string.digits
@@ -18,16 +18,17 @@ def insertintomydb(data):
         with db.cursor() as cursor:
             cursor.execute(create_table_sql)
             db.commit()
-            
+
             i=0
             for values in data :
-                cursor.execute(insert_table_sql.format(keyss=i,valuess=values))
+                cursor.execute(insert_table_sql.format(keys=i,values=values))
                 i=i+1
             db.commit()
     except:
+            db.rollback()
             db.close()
             print('插入失败！')
-    
+
 if __name__ == '__main__':
     data= activation_code(200,20)
     insertintomydb(data)
