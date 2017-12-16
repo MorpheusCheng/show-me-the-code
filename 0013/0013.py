8ab91224a8fe8f0068e82a5fd9eef5c8e40c83e3
MorpheusCheng <1203969904cheng@gmail.com>
2017-12-16 19:37:00 +0800

    嘿嘿，难点主要是正则表达式

---
+++ 0013/0013.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env pyhton
+import requests,re,os
+def savePageInfo(url,position,regx):
+    html=requests.get(url).text
+    pic_url=re.findall(regx,html)
+
+    i=0
+    for each in pic_url:
+        pic = requests.get(each)
+        print(each)
+        if not os.path.isdir(position):
+            os.mkdir(position)
+        fp=open(position+'/'+str(i)+'.jpg','wb')
+        fp.write(pic.content)
+        fp.close()
+        i=i+1
+
+
+
+url='http://tieba.baidu.com/p/2166231880'
+position='./picture'
+regx=r'src="(http://imgsrc.baidu.com/forum/.*?)"'
+
+if __name__ == '__main__':
+    savePageInfo(url,position,regx)
+
